7e50cfea8337ed32dc1ffc3236c8c39aa1522474
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-10-12 14:07:17 +0100

    generate minified output too

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 browserify react-with-addons-and-tap-event-plugin.js -o react-with-addons-0.13.3.js
+
+uglifyjs --compress --output react-with-addons-0.13.3.min.js -- react-with-addons-0.13.3.js
