3eee8e4fdf5299fe2ff20ad5d528227f99162577
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2016-05-19 09:20:03 +0100

    production build of react

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 browserify react-with-addons-and-tap-event-plugin.js -o react-with-addons-15.0.1.js
-
 uglifyjs --compress --output react-with-addons-15.0.1.min.js -- react-with-addons-15.0.1.js
+
+export NODE_ENV=production
+
+browserify react-with-addons-and-tap-event-plugin.js -o react-with-addons-15.0.1-production.js
+uglifyjs --compress --output react-with-addons-15.0.1.min.js -- react-with-addons-15.0.1-production.js
